73e24a90ab451492e8226a263078ecd1102cb31a
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2023-05-15 17:15:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 
 // Secret
 const SECRET = 'samaratools'
- 
+
 const bodyParser = require('body-parser');
 
 // Middleware
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 // Routes
 app.get('/', (req, res, next) => {
-    res.json({message: "Tudo ok por aqui!"});
+    res.json({message: "Ok!"});
 })
 
 // Calls Subsequentes obrigar o JWT
@@ -27,25 +27,24 @@ app.get('/', (req, res, next) => {
 function verifyJSWT(req,res,next) {
     const token = req.headers['x-acess-token'];
 
+    //logout
     const index  = blacklist.findIndex(item => item === token);
-
-    //if(index !== -1) return res.status(401).end();
-    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    if(index !== -1) return res.status(401).end();
+    console.log("Not authorized");
 
     jwt.verify(token, SECRET, (err, decoded) => {
-        //if(err) return res.status(401).end();
-        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-
+        if(err) return res.status(401).end();
         // se tudo estiver ok, salva no request para uso posterior
+        console.log("Authorized !!");
         req.userId = decoded.userId;
         next();
     })
 }
 
+// Acesso a parte PIV do Sistema
 app.get('/clientes', verifyJSWT, (req, res) => { 
-    console.log("Retornou todos clientes!");
     console.log(req.userId + ' fez esta chamada!');
-    res.json([{id:1, nome:'samara'}]);
+    res.json([{id:1, name:'samara'}]);
 }) 
 
 // Authentication
@@ -55,16 +54,18 @@ app.post('/login', (req, res) => {
     if(req.body.user === "samara" && req.body.password === "senha") {
         // auth ok
         const token = jwt.sign({userId:1},SECRET, {expiresIn:120}); // expires in 2min
+        console.log("Login Realized !!")
         return res.json({auth:true, token});
     }
     //res.status(401).end();
     res.status(500).json({message: 'Login inválido!'});
 })
 
+// Logout
 const blacklist = [];
-
 app.post('/logout', (req, res) => {
     blacklist.push(req.headers['x-acess-token']);
+    console.log("Logout!!");
     res.json({ auth: false, token: null });
 })
 
